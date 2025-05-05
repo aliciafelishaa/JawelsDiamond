61a631aaea974f2a4a6a8e371314a316c4793f71
MikeKomari <michaelk.projects@gmail.com>
2025-05-05 09:45:09 +0700

    a

--- JawelsDiamond/Model/Model1.Designer.cs
+++ JawelsDiamond/Model/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Nikol\OneDrive - Bina Nusantara\SMT_4\Pattern Software Design\Project\JawelsDiamond\JawelsDiamond\Model\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\micha\Source\Repos\JawelsDiamond2\JawelsDiamond\Model\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
